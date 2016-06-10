d883004f0179e5398d9ee9f1609d9b0ce98692e0
Link-Arthur <2224649384@qq.com>
2016-06-10 21:00:04 +0800

    Revert "finish the window"
    This reverts commit 1f2c0cea4ca9b5644fafb146ded6318a18351a60.

--- src/frontend/zy-zombie/zy-zombie/zy_zombie.cpp
+++
@@ -1,4 +0,0 @@
-#include"zy_zombie.h"
-int main() {
-
-}
--- src/frontend/zy-zombie/zy-zombie/zy_zombie.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include"Application.h"
-#include"Application-Event.h"
-using namespace App;
-using namespace App::Event;
-
-#include"zy_type.h"
-#include"zy_render.h"
